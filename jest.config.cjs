01c654e2f0d49be63e49673b7f4228993e65b6c0
Diana Hernandez <diana.hernandez@xing.com>
2025-06-21 10:08:27 +0200

    refactor(jest): move ts-jest config from globals to transform to remove deprecation warning and follow latest best practices

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,15 +1,16 @@
 const { createDefaultPreset } = require("ts-jest");
 
-const tsJestTransformCfg = createDefaultPreset().transform;
-
 /** @type {import("jest").Config} **/
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["@testing-library/jest-dom"],
-  globals: {
-    "ts-jest": {
-      tsconfig: "tsconfig.test.json"
-    }
+  transform: {
+    "^.+\\.(ts|tsx)$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.test.json"
+      }
+    ]
   }
 };
